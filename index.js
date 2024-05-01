d0bc486bc8d50013a3305befdf9eadf8c712b4d5
sawagani1106 <m.somasoma1106@gmail.com>
2024-05-01 18:59:44 +0900

    俺がんばった

---
+++ index.js
@@ -0,0 +1,7 @@
+    const choices=document.querySelector(".choices")//選択肢要素
+    const btn=document.querySelector(".btn")//ボタン要素の所得
+    const enemy=document.querySelector(".enemy")//敵の選択を表示する要素の所得
+    const result=document.querySelector(".result")//結果表示の要素の所得
+
+    //グーチョキパーの配列
+    const arry=["グー","チョキ","パー"]
